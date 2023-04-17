b20c5e98e2961011b896d8e41e6ec1f6d0e7f611
Satveer <Chhokar.Satveer@student.greenriver.edu>
2023-04-16 19:31:08 -0700

    added new sentence for assignment

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
 # Created 
+
+# print hello world
+print(' Hello <Satveer> ')
+# Ask user for their name
+user_name = input ("Please enter your name: ")
+# print hello to the user
+print ( "Hello " + user_name)
+print ("Today is going to be a good day!",user_name)
