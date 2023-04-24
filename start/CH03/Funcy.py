1020f01d6b16990e856a59c98acc9386a14dfa8e
Satveer <Chhokar.Satveer@student.greenriver.edu>
2023-04-23 20:59:06 -0700

    finished even script

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Satveer C
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+        print("{0} is not Even".format(num))
