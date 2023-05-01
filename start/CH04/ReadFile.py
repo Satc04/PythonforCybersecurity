f8a21184d2ba238dcbab473826a461d257cfda89
Satveer <Chhokar.Satveer@student.greenriver.edu>
2023-04-30 21:57:49 -0700

    finished adding each set of questions

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By 
+# By satveer 4/25
+
+# open file for reading
+test_file = open("hackme.txt", "r")
+
+# Read contents and print
+file_contents = test_file.read()
+print(file_contents)
+
+# Close file
+test_file.close()
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By satveer 4/25
+
+# Open file for writing
+test_file = open("hackme.txt", "w")
+
+# Ask user for name
+user_name = input ("what is your name? ")
+user_name = input ("what is your favorite color? ")
+user_name = input ("what was your first pet's name? ")
+user_name = input ("what is your mother's maiden name? ")
+user_name = input ("what elementary school did you attend? ")
+# write lines to the file
+test_file.write(f"Hello {user_name}\n")
+test_file.write("Hello {0}\n".format(user_name))
+test_file.write("Hello " + user_name + "\n")
+
+# Close teh file
+test_file.close()
+
+
+
